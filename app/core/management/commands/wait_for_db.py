757a1f908dfb1b6e7e0cfa233d7124c154a614ab
Utkarsh Tiwari <ktiwari@Utkarshs-Mac-mini.local>
2024-07-29 21:02:57 +0530

    Till Lecture 55

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,4 +16,4 @@ class Command(BaseCommand):
             except (Psycopg20pError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available!...'))
+        self.stdout.write(self.style.SUCCESS('Database available! '))
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 from django.contrib.auth.models import (
@@ -7,10 +8,20 @@ from django.contrib.auth.models import (
 )
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email = email, **extra_fields)
+    def create_user(self, email, password=None,  **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email = self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
 
         return user
 
